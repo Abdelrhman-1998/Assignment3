61b0766119d8c6f3d97238a395220e0f9a14f961
Abdelrhman-1998 <abdelrhman.ashraf.pro@gmail.com>
2022-05-23 06:43:16 +0200

    assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username:string="";
+  passwordStaus:boolean=false;
+  click:number=0;
+  clicks=[];
+  onClickButton(){
+    ++this.click;
+    this.clicks.push(this.click);
+    this.passwordStaus=!this.passwordStaus;
+  }
   resetInput(){
     this.username="";
   }
